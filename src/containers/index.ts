565ce89285e02085307daed8c02191551382c3b3
humanscape-josh <josh@humanscape.io>
2021-12-29 22:24:00 +0900

    feat: add digest list view

---
+++ src/containers/index.ts
@@ -0,0 +1,1 @@
+export { default as DigestsContainer } from './DigestsContainer';
--- src/pages/DigestListPage.tsx
+++ src/pages/DigestListPage.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import { DigestsContainer } from '../containers';
 
-const DigestListPage: FC = () => <div>digest list page</div>;
+const DigestListPage: FC = () => (
+  <div>
+    <DigestsContainer />
+  </div>
+);
 
 export default DigestListPage;
