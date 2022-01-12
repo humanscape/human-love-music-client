c438c6cc3161061872bb0a2b2aed2c7994cc2683
humanscape-josh <josh@humanscape.io>
2022-01-12 10:51:46 +0900

    style: prettier

--- src/utils/volume.ts
+++ src/utils/volume.ts
@@ -1,6 +1,7 @@
 export const getInitialVolume = () => {
   const volume = localStorage.getItem('volume');
   return (volume && Number.parseInt(volume, 10)) || 100;
-}
+};
 
-export const saveVolume = (value: number) => localStorage.setItem('volume', value.toString());
+export const saveVolume = (value: number) =>
+  localStorage.setItem('volume', value.toString());
